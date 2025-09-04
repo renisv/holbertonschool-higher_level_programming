9863728f500bff0c79bc05f172b69757b9d7257e
renisv <11432@holbertonstudents.com>
2025-09-04 09:12:52 +0000

    update

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as operation
+from add_0 import add
 if __name__ == "__main__":
 	a = 1
 	b = 2
-	print("{} + {} = {}".format(a, b, operation.add(a, b)))
+	print("{} + {} = {}".format(a, b, add(a, b)))
