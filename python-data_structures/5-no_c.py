3086594ff29134a2b414622f901417a1b9a6c20d
renisv <11432@holbertonstudents.com>
2025-09-08 12:05:40 +0000

    update

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = []
+    new_string = ""
     for element in my_string:
         if element != "c" and element != "C":
-            new_string.append(element)
+            new_string += element
 
     return new_string
