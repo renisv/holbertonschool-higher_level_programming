6aeaaa0733aa0e6a15aec9e757cba79cf1b40b93
renisv <11432@holbertonstudents.com>
2025-09-08 11:53:59 +0000

    update

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = []
+    for element in my_string:
+        if element != "c" or "C":
+            new_string.append(element)
+    return new_string
