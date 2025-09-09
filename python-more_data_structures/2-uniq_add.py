8cf41bc6abe4773edfff6873cf5ebedb041fccbf
renisv <11432@holbertonstudents.com>
2025-09-09 14:01:35 +0000

    update

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,8 +2,7 @@
 
 def uniq_add(my_list=[]):
     sum_of_digits = 0
-    firstly_set = set(my_list)
-    secondly_list = list(firstly_set)
-    for digit in secondly_list:
+    make_a_set = set(my_list)
+    for digit in make_a_set:
         sum_of_digits += digit
     return sum_of_digits
