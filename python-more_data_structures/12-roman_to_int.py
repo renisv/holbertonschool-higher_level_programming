6f90ecf2ff251d31617fd2d72fb233e9b6e08e22
renisv <11432@holbertonstudents.com>
2025-09-10 08:33:55 +0000

    update

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if len(roman_string) == 0:
+        return None
     roman_dict = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
     number = 0
     for i in range(len(roman_string)):
