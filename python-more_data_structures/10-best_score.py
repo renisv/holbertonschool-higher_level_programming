474ea6ab8aac0a58f48a7afabac3a28c6965fe96
renisv <11432@holbertonstudents.com>
2025-09-10 09:27:17 +0000

    update

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    
+    best_key = 0
+    best_value = 0
+    
+    for key, value in a_dictionary.items():
+        if best_value == 0 or value > best_value:
+            best_value = value
+            best_key = key
+    return best_key
