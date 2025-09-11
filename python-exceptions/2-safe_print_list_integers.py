b01a871ac79b7c48bdc3a373f495b1d914c7edb0
renisv <11432@holbertonstudents.com>
2025-09-11 11:53:44 +0000

    update

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,6 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
             except (ValueError, TypeError):
                 pass
     except IndexError:
-        pass
+        raise
     print()
     return count
