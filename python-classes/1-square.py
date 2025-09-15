1a8b0fdf24e7a26a2e3649b4b5756ae35f4d7f48
renisv <11432@holbertonstudents.com>
2025-09-15 09:40:52 +0000

    update

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" This creates a class square with private attribute size """
+
+
 class Square:
+    """  This creates a class square with private attribute size """
     def __init__(self, size = 0):
         self.__size = size
