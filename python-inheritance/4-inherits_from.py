08fc00f7ebb2231784a085182b5edcb6fff2572b
renisv <11432@holbertonstudents.com>
2025-09-22 11:32:05 +0000

    update

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 
 def inherits_from(obj, a_class):
     """a class that inherited """
-    if isinstance(obj, a_class) and type(obj)is not a_class:
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     return False
