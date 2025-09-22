e1f47200bdc8b9a38cea7be62114e2de45f76da3
renisv <11432@holbertonstudents.com>
2025-09-22 13:56:46 +0000

    update

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Define class Square """
+
+
+Rectangle = __import__(9-rectangle.py).Rectangle
+
+
+class Square(Rectangle):
+
+    """define a  square"""
+
+    def __init__(self, size):
+        Rectangle.integer_validator(self, "size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Func to calculate area"""
+        return self.__size ** 2
