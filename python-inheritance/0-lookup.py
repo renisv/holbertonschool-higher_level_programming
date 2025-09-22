87713cc9bdcbcf79456b9cb813bb397841ace77a
renisv <11432@holbertonstudents.com>
2025-09-22 11:09:48 +0000

    update

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+"""lookup returns the list of available attributes and methods of an object"""
 def lookup(obj):
+    """lookup returns the list of available attributes and methods of an object"""
     return dir(obj)
