0f0fbf650c651c15380462a6cf7504f29410425f
renisv <11432@holbertonstudents.com>
2025-09-22 13:58:58 +0000

    update

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" Define class Square """
+"""Module about a square"""
 
 
-Rectangle = __import__(9-rectangle.py).Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-
     """Class that implements a square"""
-
     def __init__(self, size):
         Rectangle.integer_validator(self, "size", size)
         super().__init__(size, size)
