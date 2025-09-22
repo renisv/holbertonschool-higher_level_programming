67ab00f69401fa3678a58c96e9a7fa4fef24ff01
renisv <11432@holbertonstudents.com>
2025-09-22 11:37:10 +0000

    update

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""not empty class"""
+
+
+class BaseGeometry:
+    """not empty class"""
+    def area(self):
+        """"area method"""
+        raise Exception("area() is not implemented")
