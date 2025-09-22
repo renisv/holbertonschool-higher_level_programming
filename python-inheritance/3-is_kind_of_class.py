0f5b4c40931047c71bea91b39060470fbc5c4d87
renisv <11432@holbertonstudents.com>
2025-09-22 11:27:28 +0000

    update

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""an instance of a class that inherited from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """object is an instance of"""
+    return isinstance(obj, a_class)
