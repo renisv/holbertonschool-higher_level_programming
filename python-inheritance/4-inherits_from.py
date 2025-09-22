28ad5f84833c3742f019cccc73465f7a3ddbbdec
renisv <11432@holbertonstudents.com>
2025-09-22 11:31:31 +0000

    update

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" (directly or indirectly)"""
+
+
+def inherits_from(obj, a_class):
+    """a class that inherited """
+    if isinstance(obj, a_class) and type(obj)is not a_class:
+        return True
+    return False
