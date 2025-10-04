201209b03cb9c56ddaad03f54c7d283e2beb41af
renisv <vukajrenis@gmail.com>
2025-10-04 20:47:48 +0000

    update

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -2,15 +2,19 @@
 
 import csv
 import json
-"""reading csv data and convering to json"""
 
 
 def convert_csv_to_json(csv_file):
-    """reading csv data and convering to json"""
+    """reading csv data and converting to json"""
     try:
         with open(csv_file, "r") as file:
-            csv_dict = csv.DictReader(file)
-            json.dump(csv.DictReader(file), file)
-            return True
+            csv_reader = csv.DictReader(file)
+            data = list(csv_reader)
+
+        with open("data.json", "w") as json_file:
+            json.dump(data, json_file, indent=4)
+
+        return True
+
     except FileNotFoundError:
         return False
