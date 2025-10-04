7c85ec7d6cb3666579dbdec86301a620617d1b5e
renisv <vukajrenis@gmail.com>
2025-10-04 19:54:44 +0000

    update

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,19 +6,11 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     """json serialization"""
-    try:
-        with open(filename, "w", encoding="UTF-8") as file:
-            json.dump(data, file)
-    except FileNotFoundError:
-        print(f"{filename} not found")
+    with open(filename, 'w') as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
     """json deserialization"""
-    try:
-        with open(filename, "r", encoding="UTF-8") as file:
-            python_object = json.load(file)
-            return python_object
-    except FileNotFoundError:
-        print(f"{filename} not found")
-        return {}
+    with open(filename, 'r') as file:
+        return json.load(file)
