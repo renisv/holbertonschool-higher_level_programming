625a0c3b774d6cde2be9b8f8b3faf30ae68cdcef
renisv <vukajrenis@gmail.com>
2025-10-04 20:11:32 +0000

    update

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import pickle
+"""module that serializes and deserializes with pickle"""
+
+
+class CustomObject:
+    """object made for testing pickle implementation"""
+    def __init__(self, name, age, is_student):
+        """initializing custom object"""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """displaying object"""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """pickle serialization"""
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """pickle deserialization"""
+        with open(filename, "rb") as file:
+            python_object = pickle.load(file)
+            return python_object
