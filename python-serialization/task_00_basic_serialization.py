1dbd4782d91dca1c2e61af570a1383f47eaac59c
renisv <vukajrenis@gmail.com>
2025-10-04 19:45:27 +0000

    update

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import json
+"""module that implements json serialization and deserialization"""
+
+
+def serialize_and_save_to_file(data, filename):
+    """json serialization"""
+    try:
+        with open(filename, "w", encoding="UTF-8") as file:
+            json.dump(data, file)
+    except FileNotFound:
+        print(f"{filename} not found")
+
+
+def load_and_deserialize(filename):
+    """json deserialization"""
+    try:
+        with open(filename, "r", encoding="UTF-8")as file:
+            python_object = json.load(file)
+    except FileNotFound:
+        print(f"{filename} not found")
