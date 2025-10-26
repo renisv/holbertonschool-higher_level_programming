5b1cac7db8703f8b299e44899c09c521c280f307
renisv <vukajrenis@gmail.com>
2025-10-26 17:53:34 +0000

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """
-Lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
+Lists all states from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    # Connect to the database
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3])
+if __name__ == "__main__":
+    # Connect to MySQL server
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=argv[1], passwd=argv[2], db=argv[3])
+
     cursor = conn.cursor()
-    
-    # Execute query – filter names starting with 'N'
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    # Retrieve all states sorted by id in ascending order
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
-    
-    # Print results
+
     for row in rows:
         print(row)
-    
-    # Clean up
+
     cursor.close()
     conn.close()
